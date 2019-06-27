f1c98867fbd3d58bfaff5016677e60a04f46907e
Chengqian Che <chengqian@Chengqians-MacBook-Pro.local>
2019-06-27 15:12:57 -0400

    RGB supported

--- process_exr.py
+++ process_exr.py
@@ -7,18 +7,29 @@ fn = sys.argv[1]
 tmp_fn = sys.argv[2]
 
 m_file = pyexr.open(fn)
+
 channels = m_file.channels
 
 img = np.zeros(m_file.get().shape)
 
 names =[]
-img[:, :, 0] = m_file.get('Forward')[:, :, 0]
-names += ['Forward']
-idx = 1
-for i in range(len(channels)):
-    if channels[i] != 'Forward':
-        img[:, :, idx] = m_file.get(channels[i])[:, :, 0]
-        names += [channels[i]]
-        idx += 1
+
+if (len(channels) == 1):
+        img[:, :, 0] = m_file.get(channels[0])[:, :, 0]
+        names =[channels[0]]
+elif 'R' in channels:
+        img[:, :, 0] = m_file.get('R')[:, :, 0]
+        img[:, :, 1] = m_file.get('G')[:, :, 0]
+        img[:, :, 2] = m_file.get('B')[:, :, 0]
+        names += ['R', 'G', 'B']
+elif 'Forward' in channels:
+        img[:, :, 0] = m_file.get('Forward')[:, :, 0]
+        names += ['Forward']
+        idx = 1
+        for i in range(len(channels)):
+                if channels[i] != 'Forward':
+                        img[:, :, idx] = m_file.get(channels[i])[:, :, 0]
+                        names += [channels[i]]
+                        idx += 1
 
 io.savemat(tmp_fn, {'img':img, 'channels':names})
